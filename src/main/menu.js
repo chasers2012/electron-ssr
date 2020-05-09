b6cd7186fe50e6c1e74349af3fec7da33c4ead31
Cosine <cooosine@outlook.com>
2020-05-09 13:52:12 +0800

    [MOD] removing 'else' in menu.js

--- src/main/menu.js
+++ src/main/menu.js
@@ -1,19 +1,16 @@
-import { app, Menu } from 'electron';
-import { appConfig$, currentConfig } from './data';
-import { changeProxy } from './tray';
+import {app, Menu} from 'electron';
+import {appConfig$, currentConfig} from './data';
+import {changeProxy} from './tray';
 import * as handler from './tray-handler';
-import { checkUpdate } from './updater';
-import { isMac, isLinux } from '../shared/env';
+import {checkUpdate} from './updater';
+import {isMac, isLinux} from '../shared/env';
 
 let showLinuxMenu = false;
-/**
- * 渲染菜单
- */
-export default function renderMenu(appConfig) {
+
+function genMenuTemplate(appConfig) {
   // mac需要加上默认的一些菜单，否则没法复制粘贴
-  let template;
   if (isMac) {
-    template = [{
+    return [{
       label: app.getName(),
       submenu: [
         { role: 'about' },
@@ -34,63 +31,97 @@ export default function renderMenu(appConfig) {
         { role: 'selectall' },
       ],
     }];
-  } else if (isLinux) {
-    if (showLinuxMenu) {
-      template = [
-        {
-          label: '应用',
-          submenu: [
-            {
-              label: '开启应用', type: 'checkbox', checked: appConfig.enable, click: handler.toggleEnable,
-            },
-            { label: '二维码扫描', click: handler.scanQRCode },
-            { label: '复制http代理设置', click: handler.copyHttpProxyCode },
-            { label: '退出', click: handler.exitApp },
-          ],
-        },
-        {
-          label: '系统代理模式',
-          submenu: [
-            {
-              label: '不启用代理', type: 'checkbox', checked: appConfig.sysProxyMode === 0, click: (e) => changeProxy(e, 0, appConfig),
-            },
-            {
-              label: 'PAC代理', type: 'checkbox', checked: appConfig.sysProxyMode === 1, click: (e) => changeProxy(e, 1, appConfig),
-            },
-            {
-              label: '全局代理', type: 'checkbox', checked: appConfig.sysProxyMode === 2, click: (e) => changeProxy(e, 2, appConfig),
-            },
-          ],
-        },
-        {
-          label: 'PAC',
-          submenu: [
-            { label: '更新PAC', click: handler.updatePac },
-          ],
-        },
-        {
-          label: '配置',
-          submenu: [
-            { label: '导入gui-config.json文件', click: handler.importConfigFromFile },
-            { label: '导出gui-config.json文件', click: handler.exportConfigToFile },
-            { label: '打开配置文件', click: handler.openConfigFile },
-          ],
-        },
-        {
-          label: '帮助',
-          submenu: [
-            { label: '检查更新', click: () => checkUpdate(true) },
-            { label: '查看日志', click: handler.openLog },
-            { label: '项目主页', click: () => { handler.openURL('https://github.com/erguotou520/electron-ssr'); } },
-            { label: 'Bug反馈', click: () => { handler.openURL('https://github.com/erguotou520/electron-ssr/issues'); } },
-            { label: '捐赠', click: () => { handler.openURL('https://github.com/erguotou520/donate'); } },
-            { label: '打开开发者工具', click: handler.openDevtool },
-          ],
-        },
-      ];
-    }
   }
-  template && Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  if (isLinux && showLinuxMenu) {
+    return [
+      {
+        label: '应用',
+        submenu: [
+          {
+            label: '开启应用', type: 'checkbox', checked: appConfig.enable, click: handler.toggleEnable,
+          },
+          { label: '二维码扫描', click: handler.scanQRCode },
+          { label: '复制http代理设置', click: handler.copyHttpProxyCode },
+          { label: '退出', click: handler.exitApp },
+        ],
+      },
+      {
+        label: '系统代理模式',
+        submenu: [
+          {
+            label: '不启用代理',
+            type: 'checkbox',
+            checked: appConfig.sysProxyMode === 0,
+            click: (e) => changeProxy(e, 0, appConfig),
+          },
+          {
+            label: 'PAC代理',
+            type: 'checkbox',
+            checked: appConfig.sysProxyMode === 1,
+            click: (e) => changeProxy(e, 1, appConfig),
+          },
+          {
+            label: '全局代理',
+            type: 'checkbox',
+            checked: appConfig.sysProxyMode === 2,
+            click: (e) => changeProxy(e, 2, appConfig),
+          },
+        ],
+      },
+      {
+        label: 'PAC',
+        submenu: [
+          { label: '更新PAC', click: handler.updatePac },
+        ],
+      },
+      {
+        label: '配置',
+        submenu: [
+          { label: '导入gui-config.json文件', click: handler.importConfigFromFile },
+          { label: '导出gui-config.json文件', click: handler.exportConfigToFile },
+          { label: '打开配置文件', click: handler.openConfigFile },
+        ],
+      },
+      {
+        label: '帮助',
+        submenu: [
+          { label: '检查更新', click: () => checkUpdate(true) },
+          { label: '查看日志', click: handler.openLog },
+          {
+            label: '项目主页',
+            click: () => {
+              handler.openURL('https://github.com/erguotou520/electron-ssr');
+            }
+          },
+          {
+            label: 'Bug反馈',
+            click: () => {
+              handler.openURL('https://github.com/erguotou520/electron-ssr/issues');
+            }
+          },
+          {
+            label: '捐赠',
+            click: () => {
+              handler.openURL('https://github.com/erguotou520/donate');
+            }
+          },
+          { label: '打开开发者工具', click: handler.openDevtool },
+        ],
+      },
+    ];
+  }
+  return null;
+}
+
+/**
+ * 渲染菜单
+ */
+export default function renderMenu(appConfig) {
+  const template = genMenuTemplate(appConfig);
+  if (!template) {
+    return;
+  }
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 /**
@@ -101,10 +132,10 @@ export function toggleMenu() {
     if (Menu.getApplicationMenu()) {
       showLinuxMenu = false;
       Menu.setApplicationMenu(null);
-    } else {
-      showLinuxMenu = true;
-      renderMenu(currentConfig);
+      return;
     }
+    showLinuxMenu = true;
+    renderMenu(currentConfig);
   }
 }
 
@@ -113,7 +144,9 @@ appConfig$.subscribe((data) => {
   const [appConfig, changed] = data;
   if (!changed.length) {
     renderMenu(appConfig);
-  } else if (['enable', 'sysProxyMode'].some((key) => changed.indexOf(key) > -1)) {
+    return;
+  }
+  if (['enable', 'sysProxyMode'].some((key) => changed.indexOf(key) > -1)) {
     renderMenu(appConfig);
   }
 });
